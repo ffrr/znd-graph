e0be5002661a73abdbffd4e1acf32507182b3596
František Karika <f@faux.sk>
2016-04-23 22:40:32 +0200

    removed c3 from paths

--- app/config.js
+++ app/config.js
@@ -1,12 +1,11 @@
 "use strict";
 require.config({
   // make components more sensible
-  // expose jquery 
+  // expose jquery
   paths: {
     "components": "../bower_components",
     "jquery": "../bower_components/jquery/dist/jquery",
     "d3": "../bower_components/d3/d3",
-    "c3": "../bower_components/c3/c3",
     "d3-tip": "../bower_components/d3-tip/index",
     "lodash": "../bower_components/lodash/lodash.min"
   },
@@ -16,8 +15,3 @@ require.config({
 if (window.requireTestMode) {
   require(['test'], function(){ });
 }
-
-
-
-
-
