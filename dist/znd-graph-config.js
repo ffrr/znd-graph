771a12abd475a2dde4717561de138d91988e176a
František Karika <f@faux.sk>
2017-02-13 23:50:33 +0100

    added dist release

---
+++ dist/znd-graph-config.js
@@ -0,0 +1,24 @@
+define("znd-graph-config", [], function() {
+    "use strict";
+    var config = {
+        colors: [ "00d4ff", "ff92d7", "ffc900", "00c367", "ada9ff", "fff300",
+        "ff4dd8", "007cff", "ff8600", "00acd5", "ff4d00", "00d21d",
+        "b97aff", "00deb9", "c7344f" ],
+
+        spritesPath: "system/svg/sprite.svg",
+
+        containerSelector: "#graph",
+        controlContainerSelector: "#pie",
+        navigationContainerSelector: ".navigable",
+
+        groupingAggregateName: "Ostatné",
+        visibleSegments: 5,
+        groupingThreshold: 3,
+
+        pieHeight: 30,
+        barHeight: 400,
+        timelineItemHeight: 50
+    };
+
+    return config;
+});
